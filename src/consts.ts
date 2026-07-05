aa7ff90961363b191d5108a4c704ce3cdb7089c4
igabaiRuth <77741551+igabaiRuth@users.noreply.github.com>
2026-07-06 01:24:37 +0900

    feat: redesign UI to Notion-like Mint Green app interface

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = '週5時間おとうさんプログラマー';
+export const SITE_DESCRIPTION = '家族との時間を大切にしながら、プログラミングで人生を豊かにするブログ';
